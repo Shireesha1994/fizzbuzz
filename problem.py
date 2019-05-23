340615fd0c331b1cc6554d22c36ce21c09155e44
Shireesha1994 <Smekarthi@gmail.com>
2019-05-23 09:44:41 -0300

    Creating repo and solving prb

--- problem.py
+++ problem.py
@@ -1,13 +1,17 @@
+#creating Funtion for printing 100 numbers
 def fin(num=101):
+#checking condition for divisible by 3
     for i in range(num):
         if(i % 3 == 0):
          	    print("Fizz")
 	else:
 		    print(i)
+#checking condition for divisible by 5
 	if(i % 5 == 0):
                     print("Buzz")
 	else:
-	            print("Fin")
+	            print(i)
+#checking condition for divisible by 3 and 5
 	if(i%3==0)and(i%5==0):
 		    print("FizzBuzz")
 	else:
